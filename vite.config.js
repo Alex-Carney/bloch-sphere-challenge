819fb7aa80441ea463432c52f287b9605bf32953
Alex Carney <alexander.s.carney.th@dartmouth.edu>
2024-04-28 00:08:45 -0400

    Activate actions

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    base: '/bloch-sphere-challenge/',  // Adjust 'repository-name' to match your GitHub repository name
+})
